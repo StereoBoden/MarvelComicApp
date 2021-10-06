28c9b6704a118850f73ee8ac349cedf2074c02a4
Jaime Boden <jaimeboden91@gmail.com>
2021-10-06 17:09:36 +0100

    Initial Project Commit

---
+++ app/src/main/java/com/jaime/marvelviewer/util/Constants.kt
@@ -0,0 +1,12 @@
+package com.jaime.marvelviewer.util
+
+object Constants {
+    const val BASE_URL = "https://gateway.marvel.com/"
+    const val DATABASE_NAME = "Marvel_Database"
+    const val COMIC_DETAIL_SPAN_SIZE = 2
+    const val SPLASH_SCREEN_DELAY = 3000L
+
+    // TODO: Register at https://developer.marvel.com/account to get keys
+    const val API_KEY = ""
+    const val PRIVATE_KEY = ""
+}
